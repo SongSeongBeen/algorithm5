304c24f54f8e57048c252b2770097c099a12ae7b
송성빈 <aktm1004@naver.com>
2022-10-15 13:42:52 +0900

    ch01

--- src/baekjoon/Ch01.java
+++ src/baekjoon/Ch01.java
@@ -6,14 +6,16 @@ public class Ch01 {
 	public static void main(String[] args) {
 		//1172번
 		Scanner sc = new Scanner(System.in);
+		System.out.println("몇자리의 수입니까?");
 		int N =  sc.nextInt();
+		System.out.println("숫자를입력해주세요");
 		String sNum = sc.next();
 		char[] cNum = sNum.toCharArray();
 		sc.close();
 		
 		int sum = 0;
 		for(int i = 0; i < cNum.length; i++) {
-			sum =+ cNum[i] - '0';//아키코드 숫자의 코드값차이 -	
+			sum += cNum[i] - '0';//아키코드 숫자의 코드값차이 -	
 		}
 		System.out.println(sum);
 	}
